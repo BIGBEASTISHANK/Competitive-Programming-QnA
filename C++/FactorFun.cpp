5513030964c19122569240538d2e18e8c8abfeaa
BIGBEASTISHANK <antihack@bigbeastishank.com>
2023-12-06 01:38:17 +0530

    c++: FactorFun

---
+++ C++/FactorFun.cpp
@@ -0,0 +1,94 @@
+// Question Url:
+// https://mycode.prepbytes.com/contest/PRIMETIMEJULY20/problems/FFUN
+
+// Header files
+#include <cmath>
+#include <ios>
+#include <iostream>
+#include <limits>
+
+// Variables
+int arraySize{0};
+int *inputArray{nullptr};
+int *outputArray{nullptr};
+
+// define Functions
+void takeInput();
+void calculateOutput();
+
+// Main function
+int main()
+{
+  // Taking input
+  takeInput();
+  // Calculating output
+  calculateOutput();
+
+  // Giving answer
+  std::cout << "Answer: ";
+  for (int i{0}; i < arraySize; i++)
+  {
+    std::cout << outputArray[i] << " ";
+  }
+
+  return 0;
+}
+
+// use function
+// Take input
+void takeInput()
+{
+  // Taking array size
+  std::cout << "Enter array size: ";
+  std::cin >> arraySize;
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  // Setting array size
+
+  inputArray = new int[arraySize];
+  outputArray = new int[arraySize];
+
+  if (1 <= arraySize && arraySize <= pow(10, 5))
+  {
+    // Taking array input
+    std::cout << "Enter array inputs seprated by space: ";
+    for (int i{0}; i < arraySize; i++)
+    {
+      std::cin >> inputArray[i];
+    }
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    // Constraints 2 checking array data size
+    for (int i = 0; i < arraySize; i++)
+    {
+      if (1 <= inputArray[i] && inputArray[i] <= pow(10, 6))
+        continue;
+      else
+      {
+        std::cout << "Array value size should be betwwn 1 and " << pow(10, 6) << ".";
+        break;
+      }
+    }
+  }
+  else
+  {
+    std::cout << "Enter array size between 1 and " << pow(10, 5)
+              << "."; // Constraints 1 checking array size
+  }
+}
+
+// Getting output
+void calculateOutput()
+{
+  outputArray[0] = 0;
+  for (int i{1}; i < arraySize; i++)
+  {
+    int count{0};
+    for (int j{0}; j < i; j++)
+    {
+      if (inputArray[i] % inputArray[j] == 0)
+        count++;
+    }
+    outputArray[i] = count;
+    count = 0;
+  }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Competitive Programming Question and Answers
+- Open the folder for the programming language you need to work with.
+- You will find the question names in the selected language folder.
+- Questions are provided in the question file as comments.
